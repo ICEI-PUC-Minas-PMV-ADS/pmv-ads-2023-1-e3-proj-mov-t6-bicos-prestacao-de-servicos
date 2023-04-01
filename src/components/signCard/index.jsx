e1bce0581819da5b59a4b65a6b37f18feb8db409
gisellenrosa <gisellenrosa@gmail.com>
2023-03-31 21:22:41 -0300

    login : - Add navigation - add routes

---
+++ src/components/signCard/index.jsx
@@ -0,0 +1,12 @@
+import { Button, Text, View } from "react-native";
+
+const signCard = () => {
+	return (
+		<View>
+			<Text>HomePage</Text>
+      <Button onPress={()=> }></Button>
+		</View>
+	);
+};
+
+export default Home;
---
+++ src/components/signCard/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+    container: {
+        height: '100%',
+        justifyContent: 'center'
+    },
+    questionText: {
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center',
+        paddingTop: 25,
+        fontFamily: 'DMSans_700Bold'
+    },
+    buttonAlign: {
+        paddingTop: 60,
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        borderRadius: 20,
+    }
+
+});
+
+export default styles;
